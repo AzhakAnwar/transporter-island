88872f74f217aeca2657f7d23e46f696ee8fe8a0
Azhak Anwar <aanwar03@i2c.com>
2023-04-28 20:13:16 +0500

    first commit

---
+++ Test cases.sql
@@ -0,0 +1,53 @@
+
+162*30 + 11 = 4,871
+
+
+
+Car Brand, Model, Year 
+
+
+
+Audi [892]
+
+<option data-option_id="1633" data-super_parent_id="[892]" value="Model-100">100</option>
+
+100
+200
+4000
+5000
+A3
+A4
+A5
+A6
+A7
+A8
+Allroad
+Q5
+Q7
+Tt
+
+100: 
+    1984
+    1985
+    1986
+    1987
+    1988
+    1989
+    1990
+    1991
+    1992
+    1993
+    1994
+
+5000:
+    1985
+    1986
+    1987
+    1988
+
+
+https://www.transponderisland.com/shop?search=&parent_applied=True&filter=Make-Audi&parent_applied=True&filter=Model-5000&parent_applied=True&filter=Year-1985
+
+
+response.xpath("normalize-space(//table[@class='table table-striped']//text())")
+
